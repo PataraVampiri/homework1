9b86f7d9b27dc04ff076bae96b348a6654a0bcde
PataraVampiri <taso.dzotsenidze2012@gmail.com>
2026-02-28 20:05:21 +0400

    first commit

---
+++ homework1/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  const username = 'taso'
+  
+
+  return (
+    <div>
+    <h1> my name is {username} </h1>
+      <p className = 'text' > Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. </p>
+      
+    </div>
+  )
+}
+
+export default App
